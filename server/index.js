bb91610409dc81fb807943f77ae5e1c57c7c635e
sergi255 <sergiuszskalski@gmail.com>
2024-02-08 15:37:37 +0100

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,           
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use('/api', createProxyMiddleware({ target: 'https://fantasy.premierleague.com', changeOrigin: true }));
+
+app.post('/home', async (req, res) => {
+    console.log(req.body);
+    res.status(200).json({ message: 'Team ID received' });
+});
+
+app.get('/about/:teamId', async (req, res) => {
+    const teamId = req.params.teamId;
+    try {
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching team data' });
+    }
+});
+
+const server = app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+    });
